a03ea75ede32c1920a15a9644f930ebe45b12185
hanseungtheshiba <ggm.t200019@ggh.goe.go.kr>
2020-10-15 11:46:02 +0900

    10월 15일 수업

---
+++ Assets/Scripts/Base/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player
+{
+    public long money;
+    public LunchKing[] lunchKings;
+}
+
+[Serializable]
+public class LunchKing
+{
+    public int level;
+    public int moneyPerSecond;
+    public int upgradeCost;
+}
